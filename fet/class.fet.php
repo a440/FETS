a1429d2ffe225278cf607e894709ae1886a7a91e
Steve Elgersma <elgersma@ece.cmu.edu>
2018-09-08 10:55:30 -0400

    skeleton setup of the app

---
+++ fet/class.fet.php
@@ -0,0 +1,17 @@
+<?php
+namespace fet;
+
+/**
+ * FET class
+ *
+ * Main application class
+ */
+class Fet
+{
+    /**
+     * Fet constructor.
+     */
+    public function __construct()
+    {
+    }
+}
